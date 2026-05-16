d2e4ceeb8387fe2cce2ee4fb3be42edcc41ed777
nethraa18nexus <nethraa1823@gmail.com>
2026-05-16 12:54:02 +0530

    Add files via upload

---
+++ dictaccessing.py
@@ -0,0 +1,14 @@
+student={
+    "name":"emma",
+    "age":18,
+    "class":"10th",
+    "city":"new york"
+}
+print("dictionary:",student)
+print("\nname:",student["name"])
+print("age:",student["age"])
+print("city:",student["city"])
+print("profession:",student["class"])
+
+print("\n all keys:",student.keys())
+print("\n all values:",student.values())
