ebc5c997fc5337a133018f73135d2ce4bd2a3b94
nethraa18nexus <nethraa1823@gmail.com>
2026-05-16 12:23:33 +0530

    Add files via upload

---
+++ acctuple.py
@@ -0,0 +1,12 @@
+my_tuple=(10,20,30,40,50)
+first=my_tuple[0]
+second=my_tuple[1]
+third=my_tuple[2]
+last=my_tuple[-1]
+second_last=my_tuple[-2]
+
+print("tuple:",my_tuple)
+print("first:",first)
+print("second:",second)
+print("last:",last)
+print("second_last:",second_last)
