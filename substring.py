d13086cdafa2c1ebf426617e2200fab74007c9bc
nethraa18nexus <nethraa1823@gmail.com>
2026-05-17 16:38:45 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,7 @@
+mainstring=input("enter the string")
+substring=input("enter the substring")
+
+if substring in mainstring:
+    print("substring is found")
+else:
+    print("substring is not found")
